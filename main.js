129349f376c564e9b290290d21cba87e6e91ab1d
Olivia <olivia.a1027@gmail.com>
2020-01-11 19:51:00 -0500

    created search functionality using regex

--- main.js
+++ main.js
@@ -1,58 +1,57 @@
 fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=c')
-  .then(response => response.json())
-  .then(drinkData => {
-    let drinkList = drinkData.drinks;
+    .then(response => response.json())
+    .then(drinkData => {
+        let drinkList = drinkData.drinks;
+        drinkList.forEach(element => {
+            new appendResults(
+                element.strDrink,
+                element.strInstructions,
+                element.strDrinkThumb
+            );
+        });
 
-    drinkList.forEach(element => {
-      $('.drinkContainer').append(
-        `<div class="drinkItem">
-            <div class="drinkName">  
-                ${element.strDrink}
-            </div>
-            <div class="drinkRecipe">
-                ${element.strInstructions}
-            </div>
-            <img class="drinkThumbnail" src="
-                ${element.strDrinkThumb}
-            ">
-        </div>`
-      );
+        onKeyUp = () => {
+            var userSearch = event.target.value.toLowerCase();
+            var regExUserSearch = new RegExp(`${userSearch}`, 'i');
+
+            var result = drinkList.filter(function(element) {
+                var searchName = element.strDrink;
+                return regExUserSearch.test(searchName);
+            });
+
+            if (result.length) {
+                $('.drinkContainer').empty();
+                result.forEach(element => {
+                    new appendResults(
+                        element.strDrink,
+                        element.strInstructions,
+                        element.strDrinkThumb
+                    );
+                });
+                console.log(result);
+            } else {
+                $('#apiResults').hide();
+                $errorMessaging.show();
+            }
+        };
     });
 
-    //Fix onKeyUp function to allow user to search through list
-
-    onKeyUp = () => {
-      let str = event.target.value.toLowerCase().substring(0, 3);
-
-      let filteredArr = drinkList.filter(e => {
-        let xSub = e.substring(0, 3).toLowerCase();
-        return e.toLowerCase().includes(str) || checkName(xSub, str);
-      });
-      if (filteredArr.length > 0) {
-        $('.drinkContainer').empty();
-        filteredArr.forEach(element => {
-          $('.drinkContainer').append('<div>' + element + '</div>');
-          $errorMessaging.hide();
-        });
-      } else {
-        $('.drinkContainer').empty();
-        $errorMessaging.show();
-      }
-    };
-
-    checkName = (name, str) => {
-      var pattern = str
-        .split('')
-        .map(x => {
-          return `(?=.*${x})`;
-        })
-        .join('');
-      var regex = new RegExp(`${pattern}`, 'g');
-      return name.match(regex);
-    };
-  });
 //*********************** Declarations ****************************** */
 
-const drinkArray = [];
-const $listItems = $('#apiResults');
 const $errorMessaging = $('.errorMessaging');
+
+function appendResults(name, instructions, thumbnail) {
+    return $('.drinkContainer').append(
+        `<div class="drinkItem">
+          <div class="drinkName">  
+              ${name}
+          </div>
+          <div class="drinkRecipe">
+              ${instructions}
+          </div>
+          <img class="drinkThumbnail" src="
+              ${thumbnail}
+          ">
+      </div>`
+    );
+}
